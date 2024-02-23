d394e05645a4444863079b0b758277b3bf29662c
hady444 <hadyhassan2312@gmail.com.com>
2024-02-23 17:05:12 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
-#Using Puppet, install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Puppet manifest to install Flask from pip3
+
+# Ensure Python3-pip package is installed
+package { 'python3-pip':
+  ensure => installed,
 }
+
+# Install Flask using pip3 with the specified version
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin', '/bin'],
+  environment => [],
+  creates     => '/usr/local/lib/python3.8/dist-packages/flask',
+}
+
