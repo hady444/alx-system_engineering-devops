131919c1a5b58fff662a07bb69b854feab02b333
hady444 <hady444@github.com>
2024-06-05 00:31:25 +0300

    file subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Module"""
-
+"""
+Contains the number_of_subscribers function
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
     r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
